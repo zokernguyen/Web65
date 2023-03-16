98210b6dec73a348bcb052ad1ac3ad3a60883a9d
Zoker <nguyenthanhtin1994@gmail.com>
2023-03-16 09:21:47 +0700

    l1-p1

--- Session 1 - NodeJS/index.js
+++ Session 1 - NodeJS/index.js
@@ -1,1 +1,37 @@
-console.log('Hello World');
+const { json } = require('express/lib/response');
+const fs = require('fs');
+const http = require('http');
+console.log(fs);
+
+const serverListener = (req, res) => {
+    switch (req.url) {
+        case '/': {
+            const htmlData = fs.readFileSync("./index.html", "utf-8");
+            res.end(htmlData);
+            break;
+        }
+
+        case "/api/users": {
+            const usersData = fs.readFileSync("./data.json", "utf-8");
+            res.end(usersData);
+            break;
+        }
+
+        case "/api/users/1": {
+            const usersData = JSON.parse(fs.readFileSync("./data.json", "utf-8"));
+            // console.log(usersData);
+            res.end(JSON.stringify(usersData.data[0]));
+        }
+
+        case 
+
+        default:
+            break;
+    }
+};
+
+const server = http.createServer(serverListener);
+
+server.listen(8080, () => {
+    console.log(`Server is running on port 8080`);
+});
