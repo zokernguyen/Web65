39877500dec9c35959bf11500399893f2a30118c
Zoker <nguyenthanhtin1994@gmail.com>
2023-03-19 17:50:08 +0700

    lesson reviewd

--- ss1-node/index.js
+++ ss1-node/index.js
@@ -2,6 +2,35 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+// fs.readFile('./hello.txt', 'utf-8', (err, data) => {
+//     if (err) {
+//         console.error(err);;
+//     } else {
+//         console.log(data);
+//     }
+// });
+
+// const data = fs.readFileSync("./hello.txt", "utf-8");
+// console.log(data);
+
+// fs.writeFile(
+//     "hello.txt",
+//     "hello-1",
+//     { flag: 'a' }, //'append' new content to existing file, of not set, the file will be overwritten.
+//     (err) => {
+//         if (err) {
+//             console.log('Loi roi!');
+//         } else {
+//             console.log("Done writing file.")
+//         };
+//     });
+
+// fs.writeFileSync("./hello.txt", "Hello 2", {
+//     flag: "a",
+// });
+
+// fs.writeFileSync("./data.js", "const a = 1; console.log(a)"); //if the file is not exist, creat a new file and write the content.
+
 const serverListener = (req, res) => {
     const userId = req.url.split('/').pop();
 
@@ -21,9 +50,13 @@ const serverListener = (req, res) => {
 
         case `/api/users/${userId}`: {
             const usersData = JSON.parse(fs.readFileSync("./data.json", "utf-8"));
-            const user = usersData.data.find((user) => user.id === Number(userId));
-            console.log(user);
-            res.end(JSON.stringify(user));
+            const idList = usersData.data.map(user => user.id);
+            if (idList.includes(Number(userId))) {
+                const user = usersData.data.find((user) => user.id === Number(userId));
+                res.end(JSON.stringify(user));
+            } else {
+                res.end(fs.readFileSync('err.html', 'utf-8'));
+            }
         }
 
         default:
