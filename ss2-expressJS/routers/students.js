6df58749d730897319a73ad80d3e49a1f390cf42
Zoker <nguyenthanhtin1994@gmail.com>
2023-03-19 22:05:04 +0700

    ss2-practice

--- ss2-expressJS/routers/students.js
+++ ss2-expressJS/routers/students.js
@@ -16,9 +16,42 @@ const studentsList = [
 ];
 
 const router = express.Router();
-router.get("/create", (req, res) => { });
+
+router.get("/create", (req, res) => {
+    studentsList.push({ id: 4, name: "Daniel" });
+});
+
 router.get("/read", (req, res) => { res.json(studentsList) });
-router.get("/edit", (req, res) => { });
-router.get("/delete", (req, res) => { });
+
+router.get("/edit", (req, res) => {
+    const idToEdit = req.query.id;
+    const newName = req.query.newName;
+
+    for (let i = 0; i < studentsList.length; i++) {
+        if (studentsList[i].id === Number(idToEdit)) {
+            studentsList[i].name = newName;
+            break;
+        }
+    }
+
+    res.json(studentsList);
+
+});
+
+router.get("/delete/:id", (req, res) => {
+    const delID = req.params.id;
+
+    for (let i = 0; i < studentsList.length; i++) {
+        if (studentsList[i].id === Number(delID)) {
+            studentsList.splice(i, 1);
+            break;
+        }
+    }
+
+    res.json(studentsList);
+
+    // let newList = studentsList.filter(student => student.id !== Number(req.params.id));
+    // res.json(newList);
+});
 
 export default router;
