4674062f513e47905789ab94c5d5473c2b042229
zoker <nguyenthanhtin1994@gmail.com>
2023-03-22 13:06:58 +0700

    querry & params

--- ss2-expressJS/index.js
+++ ss2-expressJS/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import studentsRouters from './routers/students.js';
+//const studentsRouters = require(./routers/students.js)
 import teachersRouters from './routers/teachers.js';
 import authenRouters from './routers/authen.js';
 
-const app = express();
+const app = express(); //express app wrapper
+app.use(express.json()); //to read req.body in json format.
+const port = 8080;
+
 
 app.get("/", (req, res) => {
 
@@ -15,10 +19,10 @@ app.get("/", (req, res) => {
     res.send('Homepage');
 });
 
-app.use("/students", studentsRouters);
+app.use("/students", studentsRouters);//adding relative "/students" prefix, no more repetition
 app.use("/teachers", teachersRouters);
 app.use("/authen", authenRouters);
 
-app.listen(8080, (err) => {
-    console.log('Server is running on port 8080');
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
--- ss2-expressJS/routers/students.js
+++ ss2-expressJS/routers/students.js
@@ -17,11 +17,46 @@ const studentsList = [
 
 const router = express.Router();
 
+//using POST method to create new student
+
+// router.post("/post", (req, res) => {
+//     const newID = req.body.id;
+//     const newName = req.body.name;
+//     //cả id lẫn name đề là data để thêm mới hoàn toàn, nằm trong req.body
+
+//     studentsList.push({ id: Number(newID), name: newName });
+
+//     res.json(studentsList);
+// });
+
 router.get("/create", (req, res) => {
-    studentsList.push({ id: 4, name: "Daniel" });
+    const newID = req.query.id;
+    const newName = req.query.name;
+
+    studentsList.push({ id: Number(newID), name: newName });
+
+    res.json(studentsList);
 });
 
+//using GET method to read the students list
+
 router.get("/read", (req, res) => { res.json(studentsList) });
+
+//using PUT method to update student
+
+// router.put("/:id", (req, res) => {
+//     const idToEdit = req.params.id;
+//     const newName = req.body.name;
+
+//     for (let i = 0; i < studentsList.length; i++) {
+//         if (studentsList[i].id === Number(idToEdit)) {
+//             studentsList[i].name = newName;
+//             break;
+//         }
+//     }
+
+//     res.json(studentsList);
+// });
 
 router.get("/edit", (req, res) => {
     const idToEdit = req.query.id;
@@ -33,13 +68,26 @@ router.get("/edit", (req, res) => {
             break;
         }
     }
-
     res.json(studentsList);
-
 });
 
+//use DELETE method to delete student
+
+// router.delete("/delete/", (req, res) => {
+//     const delID = req.querry.id;//id là querry để xác định đối tượng cần xóa.
+
+//     for (let i = 0; i < studentsList.length; i++) {
+//         if (studentsList[i].id === Number(delID)) {
+//             studentsList.splice(i, 1);
+//             break;
+//         }
+//     }
+
+//     res.json(studentsList);
+// });
+
 router.get("/delete/:id", (req, res) => {
-    const delID = req.params.id;
+    const delID = req.params.id; //params là các tham số nằm sau dấu ':'
 
     for (let i = 0; i < studentsList.length; i++) {
         if (studentsList[i].id === Number(delID)) {
@@ -55,3 +103,4 @@ router.get("/delete/:id", (req, res) => {
 });
 
 export default router;
+// module.exports = studentsRouters
