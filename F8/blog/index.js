0c0e357b486966e97d28119485f4a6112ceb4a93
zoker <nguyenthanhtin1994@gmail.com>
2023-03-27 10:30:15 +0700

    update

---
+++ F8/blog/index.js
@@ -0,0 +1,9 @@
+﻿const express = require("express");
+const app = express();
+const port = 6969;
+
+app.get("/", (req, res) => {
+    res.send("Welcome to my Blog");
+})
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
